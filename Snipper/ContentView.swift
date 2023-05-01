fd327462f561215b8dd6150ac41b3d6920a706fd
Fiora <fioratanbeilun@gmail.com>
2023-05-01 22:50:23 +0800

    Added 2 testing tabs to vertical menu bar

--- Snipper/ContentView.swift
+++ Snipper/ContentView.swift
@@ -8,19 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Snipper")
-        }
-        .padding()
+//        VStack {
+//            Image(systemName: "globe")
+//                .imageScale(.large)
+//                .foregroundColor(.accentColor)
+//            Text("Snipper")
+//
+//        }
+//        .padding()
+        Home()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct Home : View {
+    var body: some View{
+        HStack(spacing: 0){
+            VStack{
+                Button(action: {
+                                    
+                                }) {
+                                    
+                                    Image("menu")
+                                        .renderingMode(.original)
+                                        .resizable()
+                                        .frame(width: 22, height: 22)
+                                    
+                                }
+                                .padding(.top)
+                Button(action: {
+                                    
+                                }) {
+                                    
+                                    Image("menu")
+                                        .renderingMode(.original)
+                                        .resizable()
+                                        .frame(width: 22, height: 22)
+                                    
+                                }
+                                .padding(.top)
+            }
+        }
     }
 }
