0a35b7a814318f0396091cbe28c1424f9c15818a
Fiora <fioratanbeilun@gmail.com>
2023-05-01 21:34:05 +0800

    Initial Commit

---
+++ Snipper/SnipperApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnipperApp.swift
+//  Snipper
+//
+//  Created by Fiora Tan on 1/5/23.
+//
+
+import SwiftUI
+
+@main
+struct SnipperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
